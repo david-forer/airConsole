aab9c8cba5f7806b98b0eaf0d98d97d2a010aaef
david-forer <david.j.forer@gmail.com>
2021-09-08 00:08:36 +0900

    Original commit

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,11 @@
-const inquirer = require('inquirer');
+
 const clear = require('clear');
 const header = require('./header');
 const menu = require('./menu');
-const select = require('./select');
+
 
 clear();
 
 header.title('airConsole');
 
 menu.menuOptions();
-
-// select.mainOptions();
